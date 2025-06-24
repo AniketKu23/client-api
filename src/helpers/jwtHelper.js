2ea1daa255945131adf4b61bb244151c4298ee34
Aniket Kumar <kkaniket23@gmail.com>
2025-06-24 17:03:24 +0530

    debugging

--- src/helpers/jwtHelper.js
+++ src/helpers/jwtHelper.js
@@ -1,43 +1,33 @@
-const { referrerPolicy } = require('helmet')
-const jwt = require('jsonwebtoken')
-const { setJWT, getJWT } = require('./redisHelper')
-const { storeUserRefreshJWT } = require('../model/user/UserModel')
+const { referrerPolicy } = require("helmet");
+const jwt = require("jsonwebtoken");
+const { setJWT, getJWT } = require("./redisHelper");
+const { storeUserRefreshJWT } = require("../model/user/UserModel");
 
+const createAccessJWT = async (email, _id) => {
+  try {
+    const accessJWT = await jwt.sign({ email }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: "15m",
+    });
+    await setJWT(accessJWT, _id);
 
+    return Promise.resolve(accessJWT);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
-
-const createAccessJWT = async(email, _id) =>{
-    try {
-        const accessJWT = await jwt.sign(
-        { email }, 
-        process.env.JWT_ACCESS_SECRET,
-        {expiresIn: '15m'}
-    )
-    await setJWT(accessJWT, _id)
-
-    return Promise.resolve(accessJWT)
-    } catch (error) {
-        return Promise.reject(error)
-    }
-}
-
-const createRefreshJWT = async(email, _id) =>{
-
-    try {
-        const refreshJWT = jwt.sign(
-        { email }, 
-        process.env.JWT_REFRESH_SECRET,
-        {expiresIn: '30d'}
-    )
-        await storeUserRefreshJWT(_id, refreshJWT)
-    } catch (error) {
-        return Promise.reject(refreshJWT)
-    }
-    
-}
-
+const createRefreshJWT = async (email, _id) => {
+  try {
+    const refreshJWT = jwt.sign({ email }, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: "30d",
+    });
+    await storeUserRefreshJWT(_id, refreshJWT);
+  } catch (error) {
+    return Promise.reject(refreshJWT);
+  }
+};
 
 module.exports = {
-    createAccessJWT,
-    createRefreshJWT,
-}
+  createAccessJWT,
+  createRefreshJWT,
+};
--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,73 +1,70 @@
-const express = require("express")
-const router = express.Router()
-const {insertUser, getUserByEmail} = require("../model/user/UserModel")
-const {hashPassword, comparePassword} = require("../helpers/bcryptHelper")
-const { useRevalidator } = require("react-router-dom")
-const { createAccessJWT, createRefreshJWT } = require("../helpers/jwtHelper")
+const express = require("express");
+const router = express.Router();
+const { insertUser, getUserByEmail } = require("../model/user/UserModel");
+const { hashPassword, comparePassword } = require("../helpers/bcryptHelper");
+const { useRevalidator } = require("react-router-dom");
+const { createAccessJWT, createRefreshJWT } = require("../helpers/jwtHelper");
 
+router.all("/", (req, res, next) => {
+  // res.json({message: "return form user router"})
 
-router.all('/',(req, res, next)=>{
-    // res.json({message: "return form user router"})
-
-    next()
-})
+  next();
+});
 
 //Create new user route
-router.post('/',async(req,res)=>{
-    const{name,company,address,phone,email,password}= req.body
-    try{
-    const hashedPass = await hashPassword(password)
+router.post("/", async (req, res) => {
+  const { name, company, address, phone, email, password } = req.body;
+  try {
+    const hashedPass = await hashPassword(password);
     //hash pwd
-    const newUserObj={
-        name,
-        company,
-        address,
-        phone,
-        email,
-        password: hashedPass,
-    }
-    
-    const result = await insertUser(newUserObj)
+    const newUserObj = {
+      name,
+      company,
+      address,
+      phone,
+      email,
+      password: hashedPass,
+    };
 
-    res.json({ message: "New user created",result})
-    }catch(error){
-        res.json({ statux:'error', message: error.message })
-    }
-    
-})
+    const result = await insertUser(newUserObj);
+
+    res.json({ message: "New user created", result });
+  } catch (error) {
+    res.json({ statux: "error", message: error.message });
+  }
+});
 
 // User Sign in Router
-router.post("/login", async(req, res) => {
-    console.log(req.body)
-    const {email, password} = req.body
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
 
+  ///hash our email and compare with db
 
-    ///hash our email and compare with db
+  if (!email || !password) {
+    return res.json({ status: "error", message: "Invalid form submission" });
+  }
+  const user = await getUserByEmail(email);
 
-    if( !email || !password){
-        return res.json({ status: "error", message : "Invalid form submission"})
-    }
-    const user = await getUserByEmail(email)
+  const passFromDb = user && user._id ? user.password : null;
+  if (!passFromDb)
+    return res.json({ status: "error", message: "Invalid email or password!" });
+  const result = await comparePassword(password, passFromDb);
 
+  if (!result) {
+    return res.json({ status: "error", message: "Invalid email or password!" });
+  }
 
-    const passFromDb = user && user._id ? user.password : null
-    if(!passFromDb) 
-        return res.json({status : "error", message: "Invalid email or password!"})
-    const result = await comparePassword(password, passFromDb)
-    
-    if(!result){
-        return res.json({status : "error", message: "Invalid email or password!"})
-    }
-    
-    const accessJWT = await createAccessJWT(user.email, `${user._id}`);
+  const accessJWT = await createAccessJWT(user.email, `${user._id}`);
 
-    const refreshJWT = await createRefreshJWT(user.email, `${user._id}`)
+  const refreshJWT = await createRefreshJWT(user.email, `${user._id}`);
 
-    res.json({ status: "success", 
-        message : "Login Successful!",
-        accessJWT,
-        refreshJWT,
-    })
-})
+  res.json({
+    status: "success",
+    message: "Login Successful!",
+    accessJWT,
+    refreshJWT,
+  });
+});
 
-module.exports = router
+module.exports = router;
